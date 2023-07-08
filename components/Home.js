0e9ddb48699496d1496b5ed451555c56b100bdae
Yllies <hadjyllies@gmail.com>
2023-07-08 13:48:32 +0200

    update fetch

--- components/Home.js
+++ components/Home.js
@@ -11,7 +11,7 @@ function Home() {
   const [moviesData, setMoviesData] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/movies")
+    fetch("https://my-moviz-backend-phi.vercel.app/movies")
       .then((response) => response.json())
       .then((data) => {
         setMoviesData(data.movies);
